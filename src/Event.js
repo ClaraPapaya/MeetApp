6faa0c464eb7985de63d3cd4e390c95e5d71d9c0
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-07-19 15:02:38 +0200

    Add unit testing for number of events

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 
 class Event extends Component {
+  state = {
+    showHideDetails: false
+  };
+  handleShowHideButton = () => {
+    if (this.state.showHideDetails === true) {
+      this.setState({ showHideDetails: false });
+    } else {
+      this.setState({ showHideDetails: true });
+    }
+  };
   render() {
-    return <div></div>;
+    const { event } = this.props;
+    return (
+      <div className='event'>
+        <h1>{event.summary}</h1>
+        <p>{event.start.dateTime}</p>
+        <p className='locations'>{event.location}</p>
+
+        {this.state.showHideDetails && (
+          <div className='event-details'>
+            <h2>About the event</h2>
+            <a href={event.htmlLink}>See details on Google Calendar</a>
+            <p>{event.description}</p>
+          </div>
+        )}
+
+        <button className='show-hide-btn' onClick={() => this.handleShowHideButton()}>
+          {!this.state.showHideDetails ? 'show details' : 'hide details'}
+        </button>
+      </div>
+    );
   }
 }
 export default Event;
