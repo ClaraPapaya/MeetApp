04e21ea63a3f0aa3454dc9ae112ca631b74d2119
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-09-10 17:29:23 +0200

    Add Alert and InfoAlert component

---
+++ src/Alert.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+// Base component
+class Alert extends Component {
+  constructor(props) {
+    super(props);
+    this.color = null;
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+    };
+  }
+
+  render() {
+    return (
+      <div className="Alert">
+        <p style={this.getStyle()}>{this.props.text}</p>
+      </div>
+    );
+  }
+}
+
+// Subclass 1
+class InfoAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'blue';
+  }
+}
+
+export { InfoAlert };
