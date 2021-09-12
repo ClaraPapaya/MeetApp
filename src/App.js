24dfddf970888195b960374e73d7a789102fb6e5
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-09-12 18:11:23 +0200

    Add pie chart

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NumberOfEvents from './NumberOfEvents';
 import { WarningAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
 import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import EventGenre from './EventGenre';
 
 class App extends Component {
   state = {
@@ -94,15 +95,18 @@ class App extends Component {
         <NumberOfEvents eventValue={this.state.eventValue} updateEventValue={this.updateEventValue} />
         <WarningAlert text={this.state.infoText} className='WarningAlert' />
         <h4>Events in each city</h4>
-        <ResponsiveContainer height={400} >
-          <ScatterChart width={800} height={250} margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
-            <CartesianGrid strokeDasharray='3 3' />
-            <XAxis type='category' dataKey='city' name='city' />
-            <YAxis type='number' dataKey='number' name='number of events' allowDecimals={false} />
-            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter data={this.getData()} fill='#8884d8' />
-          </ScatterChart>
-        </ResponsiveContainer>
+        <div className='data-vis-wrapper'>
+          <EventGenre events={this.state.events} />
+          <ResponsiveContainer height={400} >
+            <ScatterChart width={800} height={250} margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+              <CartesianGrid strokeDasharray='3 3' />
+              <XAxis type='category' dataKey='city' name='city' />
+              <YAxis type='number' dataKey='number' name='number of events' allowDecimals={false} />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill='#8884d8' />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
         <EventList events={this.state.events} />
         <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
       </div>
