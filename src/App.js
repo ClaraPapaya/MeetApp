5759b5c7a378a91d721e3b25c5559a156f1b5d6f
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-09-12 16:59:23 +0200

    Add responsive container for scatterplot chart

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { WarningAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 class App extends Component {
   state = {
@@ -94,14 +94,15 @@ class App extends Component {
         <NumberOfEvents eventValue={this.state.eventValue} updateEventValue={this.updateEventValue} />
         <WarningAlert text={this.state.infoText} className='WarningAlert' />
         <h4>Events in each city</h4>
-        <ScatterChart width={730} height={250}
-          margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
-          <CartesianGrid strokeDasharray='3 3' />
-          <XAxis type='category' dataKey='city' name='city' />
-          <YAxis type='number' dataKey='number' name='number of events' allowDecimals={false} />
-          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-          <Scatter data={this.getData()} fill='#8884d8' />
-        </ScatterChart>
+        <ResponsiveContainer height={400} >
+          <ScatterChart width={800} height={250} margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+            <CartesianGrid strokeDasharray='3 3' />
+            <XAxis type='category' dataKey='city' name='city' />
+            <YAxis type='number' dataKey='number' name='number of events' allowDecimals={false} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill='#8884d8' />
+          </ScatterChart>
+        </ResponsiveContainer>
         <EventList events={this.state.events} />
         <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
       </div>
